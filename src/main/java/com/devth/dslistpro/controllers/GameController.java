23acb0a657ce1ab96e1cbae42558675c4ac1cadb
thisd92 <80495413+thisd92@users.noreply.github.com>
2023-05-11 12:55:08 -0300

    GET Game by id

--- src/main/java/com/devth/dslistpro/controllers/GameController.java
+++ src/main/java/com/devth/dslistpro/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devth.dslistpro.dto.GameDTO;
 import com.devth.dslistpro.dto.GameMinDTO;
 import com.devth.dslistpro.services.GameService;
 
@@ -21,5 +23,11 @@ public class GameController {
 	public List<GameMinDTO> findAll(){
 		List<GameMinDTO> gameList = gameService.findAll();
 		return gameList;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO game = gameService.findById(id);
+		return game;
 	}
 }
--- src/main/java/com/devth/dslistpro/services/GameService.java
+++ src/main/java/com/devth/dslistpro/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devth.dslistpro.dto.GameDTO;
 import com.devth.dslistpro.dto.GameMinDTO;
 import com.devth.dslistpro.entities.Game;
 import com.devth.dslistpro.repositories.GameRepository;
@@ -15,8 +17,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> gameList = gameRepository.findAll();
 		return gameList.stream().map(x -> new GameMinDTO(x)).toList();
 	};
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id){
+		Game game = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(game);
+		return dto;
+	}
 }
